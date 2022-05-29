5bf9603ce1b8a9d45db2a8f0a418eee5014e3d97
Ivan Savkov <ivansavkov1387@gmail.com>
2022-05-29 13:30:56 +0300

    Initial Commit

---
+++ BeliyCourse1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeliyCourse1
+//
+//  Created by Иван on 5/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
